bb8abef434456fefb366d43a518d4b5d1559b8bc
adam01-hash <muhsinx94@gmail.com>
2025-03-02 06:44:34 +0530

    Update ads.js

--- ads.js
+++ ads.js
@@ -14,3 +14,5 @@ const ShowAdButton = () => {
 
 // Export the component for use in a React project
 export default ShowAdButton;
+
+const ShowAdButton = () => { React.useEffect(() => { if (window.show_8678694) { return } const tag = document.createElement('script') tag.src = '//doodiwom.com/vignette.min.js' tag.dataset.zone = '8678694' tag.dataset.sdk = 'show_8678694' document.body.appendChild(tag) }, []) const showAd = () => { show_8678694().then(() => { alert('You have seen an ad!') }) } return <button onClick={showAd}>Show ad</button> }
